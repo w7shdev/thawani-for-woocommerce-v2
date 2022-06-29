44c7d2593f226de418aee272c43b34f8bf74a013
Muhannad Al Risi <60617324+w7shdev@users.noreply.github.com>
2022-06-29 20:39:27 +0400

    Fix styles of the App component

--- js/components/App.vue
+++ js/components/App.vue
@@ -1,10 +1,22 @@
 <template>
-    <div>
-        <Heading class="text-2xl text-green-600 mb-1" title="Thawani History sessions" />
+    <div class="container mx-auto lg:w-2/4">
+        <Heading class="text-2xl text-green-600 mb-1 mt-6" title="Thawani History sessions" />
         <p class="my-1">Listing your last payment history sessions. The order is from the latests to the oldest.</p>
+
+        <!-- divider --> 
+        <div class="mt-8"></div>
+        <!-- divider -->
+
+        <!-- session box --> 
+        <SessionBox />
+        <SessionBox />
+        <SessionBox />
+        <SessionBox />
+        
     </div>
 </template>
 
 <script setup>
 import Heading from "../common/Heading.vue"
+import SessionBox from "./Index/SessionsBox.vue"
 </script>
