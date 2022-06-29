a1dbc69aa6c88f1fb3bfa95e4972ac778aa70a1d
Muhannad Al Risi <60617324+w7shdev@users.noreply.github.com>
2022-06-29 20:38:21 +0400

    Create session List Box component

---
+++ js/components/Index/SessionsBox.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="p-4 py-6 bg-slate-50 rounded-md shadow mb-3">
+           <div class="flex justify-between items-center">
+            <div>
+                <span class="font-bold text-2xl text-gray-900 block">#89</span>
+                <span class="text-gray-500 block">Yesterday , 1am </span>
+            </div>
+            <div>
+                <span class="font-bold text-2xl text-gray-900 block">90.000</span>
+                <span class="text-gray-500 block">OMR</span>
+            </div>
+            <div>
+                <span class="font-bold text-2xl text-gray-900 block">Ali Al hinai</span>
+                <span class="text-gray-900 block">+968 9422 2244</span>
+            </div>
+            <div>
+                <button type="button" class="text-green-500 p-2 bg-green-200 px-8 text-center inline-block rounded-full border-none ">Paid</button>
+            </div>
+            <div>
+                More
+            </div>
+           </div>
+        </div>
+</template>
