8d5a2ace4f1f88e8ce28ed22b053376a237e5805
Muhannad Al Risi <60617324+w7shdev@users.noreply.github.com>
2022-07-05 19:09:51 +0400

    Make badge y-padding smaller

--- js/components/Index/SessionsBox.vue
+++ js/components/Index/SessionsBox.vue
@@ -14,7 +14,7 @@
                 <span class="text-gray-900 block">+968 9422 2244</span>
             </div>
             <div>
-                <button type="button" class="text-green-500 p-2 border-2 border-solid bg-stone-50 outline-none border-green-500 px-8 text-center inline-block rounded-full">Paid</button>
+                <button type="button" class="text-green-500 p-1 border-2 border-solid bg-stone-50 outline-none border-green-500 px-8 text-center inline-block rounded-full">Paid</button>
             </div>
             <div>
                 <button type="button" class="border-none cursor-pointer bg-slate-50 hover:bg-gray-100 p-2 rounded">
