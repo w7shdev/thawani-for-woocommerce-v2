c26cab7cb36a7e2c9beea034ea8398bc2e5fc92f
Muhannad Al Risi <60617324+w7shdev@users.noreply.github.com>
2022-07-06 20:25:50 +0400

    Add toggle for the popup

--- js/components/Index/SessionsBox.vue
+++ js/components/Index/SessionsBox.vue
@@ -17,10 +17,10 @@
                 <button type="button" class="text-green-500 p-1 border-2 border-solid bg-stone-50 outline-none border-green-500 px-8 text-center inline-block rounded-full">Paid</button>
             </div>
             <div class="relative">
-                <button type="button" class="border-none cursor-pointer bg-slate-50 hover:bg-gray-100 p-2 rounded">
+                <button @click="togglePopup" type="button" class="border-none cursor-pointer bg-slate-50 hover:bg-gray-100 p-2 rounded">
                     <Menu />
                 </button>
-                <div class="absolute p-2 bg-slate-50 rounded shadow min-w-[200px] right-0 top-8 z-10">
+                <div class="absolute p-2 bg-slate-50 rounded shadow min-w-[200px] right-0 top-8 z-10" :class="{block : state.popup, hidden: !state.popup}">
                    <div class="flex-1">
                        <div class="flex space-x-2 items-center p-2 rounded cursor-pointer hover:bg-slate-100">
                          <div>
@@ -46,4 +46,11 @@
     import Menu from "../../icons/Menu.vue"
     import Search from "../../icons/Search.vue"
     import Cash from "../../icons/Cash.vue"
+    import {reactive} from "vue";
+
+    const state  = reactive({popup: false});
+
+    const togglePopup = () => {
+        state.popup = !state.popup
+    }
 </script>
