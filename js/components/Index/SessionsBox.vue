5320423ab04fb60682d7759f16a35899e6f33e33
Muhannad Al Risi <60617324+w7shdev@users.noreply.github.com>
2022-07-06 19:42:51 +0400

    Add option popup

--- js/components/Index/SessionsBox.vue
+++ js/components/Index/SessionsBox.vue
@@ -16,10 +16,27 @@
             <div>
                 <button type="button" class="text-green-500 p-1 border-2 border-solid bg-stone-50 outline-none border-green-500 px-8 text-center inline-block rounded-full">Paid</button>
             </div>
-            <div>
+            <div class="relative">
                 <button type="button" class="border-none cursor-pointer bg-slate-50 hover:bg-gray-100 p-2 rounded">
                     <Menu />
                 </button>
+                <div class="absolute p-2 bg-slate-50 rounded shadow min-w-[200px] right-0 top-8 z-10">
+                   <div class="flex-1">
+                       <div class="flex space-x-2 items-center p-2 rounded cursor-pointer hover:bg-slate-100">
+                         <div>
+                           <Search />
+                         </div>
+                         <div>View all details</div>
+                       </div>
+
+                       <div class="flex space-x-2 items-center p-2 rounded cursor-pointer hover:bg-slate-100">
+                         <div>
+                           <Cash />
+                         </div>
+                         <div>View all details</div>
+                       </div>
+                   </div>
+                </div>
             </div>
            </div>
         </div>
@@ -27,4 +44,6 @@
 
 <script setup>
     import Menu from "../../icons/Menu.vue"
+    import Search from "../../icons/Search.vue"
+    import Cash from "../../icons/Cash.vue"
 </script>
