967bd00ab5acf228887503a6f9a64372b989b16f
Muhannad Al Risi <60617324+w7shdev@users.noreply.github.com>
2022-07-08 18:08:23 +0400

    Add a action as a parameter for the fetcher functionality

--- js/service/fetch.js
+++ js/service/fetch.js
@@ -6,8 +6,11 @@
  * @param {Object} data object payload
  * @returns Promise<any> JSON response
  */
- export async function request(data){
+ export async function request(data, action){
     const formData = new FormData();
+    const prefix  = "thawani_gw";
+
+     formData.append("action", `${prefix}_${action}`);
 
     for(const key in data){
         formData.append(key, data[key])
