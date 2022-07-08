489de92febab7e46fe4bc1b56c8aaae44b89fc49
Muhannad Al Risi <60617324+w7shdev@users.noreply.github.com>
2022-07-08 13:34:53 +0400

    Adding Filter popup

--- js/components/App.vue
+++ js/components/App.vue
@@ -6,10 +6,29 @@
         <Heading class="text-2xl text-green-600 mb-1 mt-6" title="Thawani History sessions" />
         <p class="my-1">Listing your last payment history sessions. The order is from the latests to the oldest.</p>
   </div>
-  <div>
+  <div class="relative">
     <button class="bg-gray-200 rounded hover:bg-slate-50 shadow cursor-pointer text-gray-500 p-2 px-4 border-none  inline-block">
          <span class="flex items-center"><Filter class="mr-2 h-4 w-4" /> Filter</span>
     </button>
+
+    <div class="absolute top-8 right-0 min-w-[200px] rounded shadow bg-slate-50 text-gray-500 py-2 px-4 z-10">
+       <div class="flex justify-between items-center">
+         <div>
+            Per page
+         </div>
+         <div>
+            <select name="perpage" class="min-w-[120px]">
+              <option> 10 </option>
+              <option> 25 </option>
+              <option> 40 </option>
+              <option> 50 </option>
+            </select>
+         </div>
+       </div>
+       <div class="flex justify-end mt-2">
+         <button type="button" class="px-4 p-2 bg-green-500 text-white min-w-[120px] border border-solid border-green-500 rounded hover:bg-green-600 hover:text-white cursor-pointer">UPDATE</button> 
+       </div>                         
+    </div>
   </div>
 </div>
 
