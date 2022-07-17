451a460d5aaa2db4c111bb179f36d13196ed9a31
Muhannad Al Risi <60617324+w7shdev@users.noreply.github.com>
2022-07-17 20:14:37 +0400

    Create Thawani status component

---
+++ js/components/Index/ThawaniStatus.vue
@@ -0,0 +1,24 @@
+<template>
+    <div><icon :class="classes" /></div>
+</template>
+
+<script setup>
+import Check from  "../../icons/Check.vue";
+import Ex from  "../../icons/Ex.vue";
+import Excalmation from  "../../icons/Excalmation.vue";
+const prop = defineProps(['status'])
+let classes = "";
+let icon = Check;
+
+if(prop.status.toLowerCase() === "cancelled")
+{
+        icon  = Ex; 
+        classes = "text-red-500"
+}else if (prop.status.toLowerCase() === "paid"){
+        icon = Check;
+        classes = "text-green-500"
+}else {
+        icon = Excalmation;
+        classes = "text-gray-500"
+}
+</script>
